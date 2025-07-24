a93751d537e645ca74f57ad283c42fb9a2b1f3eb
Krishnan S <kris2005f24@gmail.com>
2025-07-24 22:29:18 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import "./index.css";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
